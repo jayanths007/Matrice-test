9e3d95600f1050a6141a6f52e0358c77da6b2456
jayanths007 <jayanths007@gmail.com>
2021-09-23 08:37:56 +0200

    check

--- Matrice.js
+++ Matrice.js
@@ -7,3 +7,63 @@ const fiztes = (i) => {
       return "Boy";
     } else return i;
   };
+
+
+  const fiztes = (i) => {
+    if (i % 15 == 0) {
+      return "Boy Girl";
+    } else if (i % 3 == 0) {
+      return "Girl";
+    } else if (i % 5 == 0) {
+      return "Boy";
+    } else return i;
+  };
+  const fiztes = (i) => {
+    if (i % 15 == 0) {
+      return "Boy Girl";
+    } else if (i % 3 == 0) {
+      return "Girl";
+    } else if (i % 5 == 0) {
+      return "Boy";
+    } else return i;
+  };const fiztes = (i) => {
+    if (i % 15 == 0) {
+      return "Boy Girl";
+    } else if (i % 3 == 0) {
+      return "Girl";
+    } else if (i % 5 == 0) {
+      return "Boy";
+    } else return i;
+  };const fiztes = (i) => {
+    if (i % 15 == 0) {
+      return "Boy Girl";
+    } else if (i % 3 == 0) {
+      return "Girl";
+    } else if (i % 5 == 0) {
+      return "Boy";
+    } else return i;
+  };const fiztes = (i) => {
+    if (i % 15 == 0) {
+      return "Boy Girl";
+    } else if (i % 3 == 0) {
+      return "Girl";
+    } else if (i % 5 == 0) {
+      return "Boy";
+    } else return i;
+  };const fiztes = (i) => {
+    if (i % 15 == 0) {
+      return "Boy Girl";
+    } else if (i % 3 == 0) {
+      return "Girl";
+    } else if (i % 5 == 0) {
+      return "Boy";
+    } else return i;
+  };const fiztes = (i) => {
+    if (i % 15 == 0) {
+      return "Boy Girl";
+    } else if (i % 3 == 0) {
+      return "Girl";
+    } else if (i % 5 == 0) {
+      return "Boy";
+    } else return i;
+  };
